18ee4b29dc09584c23727941362b80977bc0a86d
Nigel Stewart <nigels@nigels.com>
2018-12-29 01:58:53 +1000

    Update icon resources for both Qt and flatpak purposes

--- package/make_icon.sh
+++ package/make_icon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 files=""
-for width in 16 32 48 256 ; do
+for width in 16 32 48 128 256 ; do
     name=tmp_icon_$(printf "%.3d" $width).png
     inkscape \
         --export-png=$name \
@@ -13,6 +13,11 @@ done
 
 convert $files displaz.ico
 
-cp tmp_icon_256.png ../src/resource/displaz_icon_256.png
+cp icon.svg         ../src/resource/icons/hicolor/scalable/apps/displaz.svg
+cp tmp_icon_016.png ../src/resource/icons/hicolor/16x16/apps/displaz.png
+cp tmp_icon_032.png ../src/resource/icons/hicolor/32x32/apps/displaz.png
+cp tmp_icon_048.png ../src/resource/icons/hicolor/48x48/apps/displaz.png
+cp tmp_icon_128.png ../src/resource/icons/hicolor/128x128/apps/displaz.png
+cp tmp_icon_256.png ../src/resource/icons/hicolor/256x256/apps/displaz.png
 
 rm $files
