226ff747a97aed633758607ecb7681f00755b72a
Nigel Stewart <nigels@nigels.com>
2024-08-17 16:20:40 +1000

    gldebug

--- src/render/View3D.cpp
+++ src/render/View3D.cpp
@@ -30,120 +30,6 @@
 #include "tinyformat.h"
 #include "util.h"
 
-void GLAPIENTRY
-MessageCallback( GLenum source,
-                 GLenum type,
-                 GLuint id,
-                 GLenum severity,
-                 GLsizei length,
-                 const GLchar* message,
-                 const void* userParam )
-{
-  fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-           ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
-            type, severity, message );
-}
-
-void APIENTRY MessageCallback2(GLenum source, GLenum type, GLuint id,
-                            GLenum severity, GLsizei length,
-                            const GLchar *msg, const void *data)
-{
-    char* _source;
-    char* _type;
-    char* _severity;
-
-    switch (source) {
-        case GL_DEBUG_SOURCE_API:
-        _source = "API";
-        break;
-
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-        _source = "WINDOW SYSTEM";
-        break;
-
-        case GL_DEBUG_SOURCE_SHADER_COMPILER:
-        _source = "SHADER COMPILER";
-        break;
-
-        case GL_DEBUG_SOURCE_THIRD_PARTY:
-        _source = "THIRD PARTY";
-        break;
-
-        case GL_DEBUG_SOURCE_APPLICATION:
-        _source = "APPLICATION";
-        break;
-
-        case GL_DEBUG_SOURCE_OTHER:
-        _source = "UNKNOWN";
-        break;
-
-        default:
-        _source = "UNKNOWN";
-        break;
-    }
-
-    switch (type) {
-        case GL_DEBUG_TYPE_ERROR:
-        _type = "ERROR";
-        break;
-
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-        _type = "DEPRECATED BEHAVIOR";
-        break;
-
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-        _type = "UDEFINED BEHAVIOR";
-        break;
-
-        case GL_DEBUG_TYPE_PORTABILITY:
-        _type = "PORTABILITY";
-        break;
-
-        case GL_DEBUG_TYPE_PERFORMANCE:
-        _type = "PERFORMANCE";
-        break;
-
-        case GL_DEBUG_TYPE_OTHER:
-        _type = "OTHER";
-        return;
-        break;
-
-        case GL_DEBUG_TYPE_MARKER:
-        _type = "MARKER";
-        break;
-
-        default:
-        _type = "UNKNOWN";
-        break;
-    }
-
-    switch (severity) {
-        case GL_DEBUG_SEVERITY_HIGH:
-        _severity = "HIGH";
-        break;
-
-        case GL_DEBUG_SEVERITY_MEDIUM:
-        _severity = "MEDIUM";
-        break;
-
-        case GL_DEBUG_SEVERITY_LOW:
-        _severity = "LOW";
-        break;
-
-        case GL_DEBUG_SEVERITY_NOTIFICATION:
-        _severity = "NOTIFICATION";
-        return;
-        break;
-
-        default:
-        _severity = "UNKNOWN";
-        break;
-    }
-
-    printf("%d: %s of %s severity, raised from %s: %s\n",
-            id, _type, _severity, _source, msg);
-}
-
 //------------------------------------------------------------------------------
 View3D::View3D(GeometryCollection* geometries, const QSurfaceFormat& format, QWidget *parent)
     : QOpenGLWidget(parent),
@@ -379,8 +265,8 @@ void View3D::initializeGL()
         return;
     }
 
-    glEnable              ( GL_DEBUG_OUTPUT );
-    glDebugMessageCallback( MessageCallback2, 0 );
+    glEnable              (GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(_debugMessageCallback, 0);
 
     glCheckError();
 
